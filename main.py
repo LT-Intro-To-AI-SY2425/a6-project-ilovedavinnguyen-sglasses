f16f5a9902cda5dbd87a8826b4484582deb74495
Adrian Lee <ajlee4@cps.edu>
2025-01-08 10:58:02 -0600

    struggling

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from ucimlrepo import fetch_ucirepo 
-  
+from ucimlrepo import fetch_ucirepo
+from sklearn.linear_model import LinearRegression 
+
 # fetch dataset 
 census_income_kdd = fetch_ucirepo(id=117) 
-  
+
 # data (as pandas dataframes) 
-X = census_income_kdd.data.features 
+x = census_income_kdd.data.features 
 y = census_income_kdd.data.targets 
   
-# metadata 
-print(census_income_kdd.metadata) 
-  
-# variable information 
-print(census_income_kdd.variables) 
+sklearn.linear_model.LinearRegression().fit(x, y)
