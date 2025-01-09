662036625a5cb7923dec44a5c462b32a8af9313b
Adrian Lee <ajlee4@cps.edu>
2025-01-09 10:31:27 -0600

    data

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import math
 
-data = pd.read_csv("census-income.csv")
-x = data["Age"].values
-y = data["Blood Pressure"].values
+data = pd.read_csv("census-income.csv") #a6-project-ilovedavinnguyen-sglasses/
+x = data["age"].values #AAGE
+y = data["total person income"].values #PTOTVAL
+
+
+model = LinearRegression().fit(x,y) 
+
+
+plt.xlabel("Age")
+plt.ylabel("total person income")
+
+plt.scatter(x, y)
+plt.plot(x, c="r", label="Line of Best Fit")
+plt.legend()
+plt.show()
