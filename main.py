bec19b38a21b97a3404c10c91cc90642e847bd8d
Adrian Lee <ajlee4@cps.edu>
2025-01-09 10:15:05 -0600

    t

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-abc
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import math
+
+data = pd.read_csv("census-income.csv")
+x = data["Age"].values
+y = data["Blood Pressure"].values
